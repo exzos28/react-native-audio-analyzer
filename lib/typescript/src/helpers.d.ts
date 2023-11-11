cebdd5382f8282a3f9016a7ed6448f0c75f79081
exzos <oleksandr.kurinnyi.dev@gmail.com>
2023-11-12 00:20:10 +0100

    fix: correct .gitignore

---
+++ lib/typescript/src/helpers.d.ts
@@ -0,0 +1,3 @@
+export declare function scale(arr: number[]): number[];
+export declare function sample(arr: number[], newSize: number): number[];
+//# sourceMappingURL=helpers.d.ts.map
---
+++ lib/typescript/src/index.d.ts
@@ -0,0 +1,7 @@
+export type AmplitudeData = {
+    amplitude: number;
+    timeInSeconds: number;
+};
+export declare function analyzeAudio(filepath: string): Promise<AmplitudeData[]>;
+export * from './helpers';
+//# sourceMappingURL=index.d.ts.map

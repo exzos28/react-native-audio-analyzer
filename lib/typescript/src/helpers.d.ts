ea7f169e87706e8cd92e0ab627bc7bea0e9726f2
exzos <oleksandr.kurinnyi.dev@gmail.com>
2023-11-16 18:01:05 +0100

    Revert "fix: correct .gitignore"
    This reverts commit cebdd5382f8282a3f9016a7ed6448f0c75f79081.

--- lib/typescript/src/helpers.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function scale(arr: number[]): number[];
-export declare function sample(arr: number[], newSize: number): number[];
-//# sourceMappingURL=helpers.d.ts.map
--- lib/typescript/src/index.d.ts
+++
@@ -1,7 +0,0 @@
-export type AmplitudeData = {
-    amplitude: number;
-    timeInSeconds: number;
-};
-export declare function analyzeAudio(filepath: string): Promise<AmplitudeData[]>;
-export * from './helpers';
-//# sourceMappingURL=index.d.ts.map
